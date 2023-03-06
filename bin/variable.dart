aaf431ded01cfb2f6baa6415d4f47cb8d690c536
Damenjo Sitepu <damenjos@thebigrichgroup.com>
2023-03-06 20:01:57 +0700

    Learn: Hello world and variable ( var ) keyword

---
+++ bin/variable.dart
@@ -0,0 +1,13 @@
+void main () {
+  // Variable biasa
+  // String yourName = "Damenjo Sitepu";
+
+  // print(yourName);
+
+  //  Variable dengan kata kunci "var"
+  var number = 10;
+  var thisIsString = 'Hello World';
+
+  print(number);
+  print(thisIsString);
+}
