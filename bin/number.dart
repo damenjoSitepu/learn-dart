5b0f6fa61de4f349117e3bd346f7e75a9e2c04a9
Damenjo Sitepu <damenstory@gmail.com>
2023-03-08 08:44:38 +0700

    Learn: number type data

---
+++ bin/number.dart
@@ -0,0 +1,12 @@
+void main() {
+  // int number1 = 20;
+  // print(number1);
+  // double number2 = 200.5;
+  // number2 = 100;
+  // print(number2);
+
+  // Num keyword
+  num number = 10;
+  number = 20.75;
+  print(number);
+}
