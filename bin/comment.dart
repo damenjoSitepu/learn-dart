0369f5519cf2cc7f7c1b1d1d4ebd2cc567eaf81e
Damenjo Sitepu <damenstory@gmail.com>
2023-03-08 08:40:56 +0700

    Learn: variable

---
+++ bin/comment.dart
@@ -0,0 +1,9 @@
+/// Comments By Dart Documentation
+void main() {
+  // Nama variable will return the name of owner
+  String nama = 'Damenjo Sitepu';
+  /**
+   * Output was printed by nama variable
+   */
+  print(nama);
+}
--- bin/variable.dart
+++ bin/variable.dart
@@ -1,13 +1,33 @@
-void main () {
+void main() {
   // Variable biasa
   // String yourName = "Damenjo Sitepu";
 
   // print(yourName);
 
   //  Variable dengan kata kunci "var"
-  var number = 10;
-  var thisIsString = 'Hello World';
+  // var number = 10;
+  // var thisIsString = 'Hello World';
 
-  print(number);
-  print(thisIsString);
+  // print(number);
+  // print(thisIsString);
+
+  // Variable dengan kata kunci "final"
+  // final firstName = 'Damenjo Sitepu';
+  // print(firstName);
+
+  // Variable dengan kata kunci "const"
+  // const arrayNum = [1, 2, 3, 4, 5];
+  // final arrayNum = [1, 2, 3, 4, 5];
+  // arrayNum[1] = 12;
+  // print(arrayNum);
+
+  // Variable dengan kata kunci "late"
+  late String hello = helloWorld();
+  print('Variable Hello telah dibuat');
+  print(hello);
+}
+
+String helloWorld() {
+  print('Hallo dunia');
+  return 'Ini adalah hello world yang di return';
 }
